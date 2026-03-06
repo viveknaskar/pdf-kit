1408925bd84ba2a4fd33a07f90f0ec23542f8783
Vivek Naskar <viveknaskar2912@gmail.com>
2026-03-07 01:35:28 +0530

    Changes GitHub Pages source to "GitHub Actions"

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.',
-  publicDir: 'public',
   base: '/pdf-kit/',
+  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
